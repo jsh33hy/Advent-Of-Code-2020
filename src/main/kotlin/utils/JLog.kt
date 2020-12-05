10b77019484341f9542be52d7652a2ebf9d01010
jsh33hy <jeremiah.sheehy@yahoo.com>
2020-12-04 19:43:22 -0600

    added debug option for logger

--- src/main/kotlin/utils/JLog.kt
+++ src/main/kotlin/utils/JLog.kt
@@ -6,7 +6,13 @@ class JLog(private val classIdentifier: String) {
     fun log(msg: String){
         println("${Instant.now()} :: [$classIdentifier] >> $msg")
     }
+    fun debug(msg: String){
+        if(DEBUG){
+            log(msg)
+        }
+    }
     companion object{
+        private val DEBUG = false
         private val defaultLogger = JLog("")
 
         fun getLogger(logIdentifier : String) : JLog{
