b2a5c74e5d698acd0a0d6f8d7cb88a4b2d0bd161
jsh33hy <jeremiah.sheehy@yahoo.com>
2020-12-04 19:50:51 -0600

    added comments for PassportProcessing

--- src/main/kotlin/day4/PassportProcessing1.kt
+++ src/main/kotlin/day4/PassportProcessing1.kt
@@ -9,12 +9,13 @@ class PassportProcessing1 : AocProblemSolver() {
 
     override fun solve(): String {
         val fileInput = getRawFileInput()
+        //  group passportData for each passport together into 1 line, separate passports by ;
         val parsedInput = fileInput.split("\n\n").map {
             //  replace spaces and new lines with ;
             it.replace("\\s+".toRegex(), ";")
         }
-        //  each line should entry should like hcl:#d125e3;iyr:2016;byr:1982;eyr:2027;hgt:154cm;pid:365548961
-
+        //  at this point each passport entry should be grouped together into 1 item and look like
+        //  hcl:#d125e3;iyr:2016;byr:1982;eyr:2027;hgt:154cm;pid:365548961
         return parsedInput.filter {
             validatePassportData(it)
         }.count().toString()
--- src/main/kotlin/day4/PassportProcessing2.kt
+++ src/main/kotlin/day4/PassportProcessing2.kt
@@ -11,12 +11,12 @@ class PassportProcessing2 : AocProblemSolver() {
 
     override fun solve(): String {
         val fileInput = getRawFileInput()
-        //  group passportData for each passport together into 1 line, separated by ;
+        //  group passportData for each passport together into 1 line, separate passports by ;
         val parsedInput = fileInput.split("\n\n").map {
             //  replace spaces and new lines with ;
             it.replace("\\s+".toRegex(), ";")
         }
-        //  at this point each passport entry should be grouped together and look like
+        //  at this point each passport entry should be grouped together into 1 item and look like
         //  hcl:#d125e3;iyr:2016;byr:1982;eyr:2027;hgt:154cm;pid:365548961
         return parsedInput.filter {
             val isValid = validatePassportData(it)
